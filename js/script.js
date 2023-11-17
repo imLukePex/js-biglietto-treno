376ed8392a89bf0d310c92c95450c246db1234f7
imLukePex <luca.pessione@gmail.com>
2023-11-17 15:23:53 +0100

    creation biglietto treno JavaScript

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+
+//     il prezzo del biglietto è definito in base ai km (0.21 € al km)
+//     va applicato uno sconto del 20% per i minorenni
+//     va applicato uno sconto del 40% per gli over 65. 
+
+// L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+// Questo richiederà un minimo di ricerca.
+
+let risultato = "";
+
+// Chiediamo a User numero chilometri che vuole percorrereù
+
+
+// Chiediamo a User la sua età
+
+// Calcoliamo il prezzo del biglietto in base ai km
+
+// Applichiamo uno sconto del 20% per i minorenni
+
+// Applichiamo uno sconto del 40% per gli over 65
+
+// Output prezzo finale con massimo due decimali
